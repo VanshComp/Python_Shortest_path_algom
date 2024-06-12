6fad701ad7277a994c9ddbe7d0376ad2ee38b516
VanshComp <vanshgautam2005@gamil.com>
2024-06-12 14:06:01 +0530

    Shortest Path Algorithm

---
+++ Graph_traversal.py
@@ -0,0 +1,38 @@
+my_graph = {
+    'A': [('B', 5), ('C', 3), ('E', 11)],
+    'B': [('A', 5), ('C', 1), ('F', 2)],
+    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
+    'D': [('C',1 ), ('E', 9), ('F', 3)],
+    'E': [('A', 11), ('C', 5), ('D', 9)],
+    'F': [('B', 2), ('D', 3)]
+}
+
+def shortest_path(graph, start, target = ''):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
+                if paths[node] and paths[node][-1] == node:
+                    paths[node] = paths[current][:]
+                else:
+                    paths[node].extend(paths[current])
+                paths[node].append(node)
+        unvisited.remove(current)
+    
+    targets_to_print = [target] if target else graph
+    for node in targets_to_print:
+        if node == start:
+            continue
+        print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
+    
+    return distances, paths
+print("'A': [('B', 5), ('C', 3), ('E', 11)],\n'B': [('A', 5), ('C', 1), ('F', 2)],\n'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],\n'D': [('C',1 ), ('E', 9), ('F', 3)],\n'E': [('A', 11), ('C', 5), ('D', 9)],\n'F': [('B', 2), ('D', 3)]")
+start=input("Enter your startting node: ")
+target=input("Enter targetting node...\nNote: You can simply input an empty string for finding all particular distances(no spaces also)...(or)\nupto which shortest distance have to be found from starting node: ")    
+shortest_path(my_graph, start,target)
